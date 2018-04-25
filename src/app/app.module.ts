b09c5d00842aef87bd1116bf98db69cd46492b22
djoho <djoho@gmx.net>
2018-04-25 09:10:48 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TicketOverviewComponent } from './components/ticket-overview/ticket-overview.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TicketOverviewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/ticket-overview/ticket-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ticket-ticket-overview',
+  templateUrl: './ticket-overview.component.html',
+  styleUrls: ['./ticket-overview.component.scss']
+})
+export class TicketOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
