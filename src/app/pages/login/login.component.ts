4939ce377b1a155fefaf7d14a3dc4af17d41a15c
djoho <djoho@gmx.net>
2018-04-26 13:54:46 +0200

    Login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+import {UserState} from '../../services/user-state.service';
+import {Router} from '@angular/router';
+import {BoardMessageService} from '../../services/board-message.service';
 
 @Component({
-  selector: 'ticket-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'ticket-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    public loginForm: FormGroup;
 
-  constructor() { }
+    constructor(private fb: FormBuilder,
+                private userService: UserService,
+                private userState: UserState,
+                private router: Router,
+                private boardMessageService: BoardMessageService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loginForm = this.fb.group({
+            email: [null, Validators.required],
+            password: [null, Validators.required]
+        });
+        let token = localStorage.getItem('token');
+        if (token) {
+            this.userService.checkToken(token).subscribe(() => {
+                this.checkLoggedIn();
+            });
+        }
+    }
 
+    public submit() {
+        if (this.loginForm.valid) {
+            this.userService.login(this.loginForm.value).subscribe((val) => {
+                this.checkLoggedIn();
+            });
+        }
+    }
+
+    private checkLoggedIn() {
+        if (this.userState.isLoggedIn) {
+            this.router.navigateByUrl('/home');
+        } else {
+            localStorage.removeItem('token');
+            this.boardMessageService.addMessage('E-Mail oder Passwort ungültig', 'error', 5000);
+        }
+    }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,11 +2,18 @@ import {Injectable} from '@angular/core';
 import {UserState} from './user-state.service';
 import {UserInterface} from '../interfaces/user.interface';
 import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/do';
+
+interface AuthAnswer {
+    user: UserInterface;
+    token: string;
+}
 
 @Injectable()
 export class UserService {
 
-    constructor(private userState: UserState, private httpClient: HttpClient) {
+    constructor(private userState: UserState,
+                private httpClient: HttpClient) {
     }
 
     public getUsers() {
@@ -27,5 +34,23 @@ export class UserService {
 
     public delete(user: UserInterface) {
         return this.httpClient.delete<UserInterface>('/api/user/' + user._id);
+    }
+
+    public login(val: { email: string, password: string }) {
+        // do wird vor subscribe ausgeführt. Wird in der Regel für Validierungslogik verwendet.
+        return this.httpClient.post<AuthAnswer>('/api/user/login', val)
+            .do(this.processAuthAnswer.bind(this));
+    }
+
+    public checkToken(token: string) {
+        return this.httpClient.post<AuthAnswer>('/api/user/checkToken', {token: token})
+            .do(this.processAuthAnswer.bind(this));
+    }
+
+    private processAuthAnswer(serverAnswer: AuthAnswer) {
+        if (serverAnswer.user) {
+            this.userState.setUser(serverAnswer.user);
+            localStorage.setItem('token', serverAnswer.token);
+        }
     }
 }
